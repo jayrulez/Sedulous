ab5d9fa366da266c04fb24ec79fcdb95b095ac38
Robert Campbell <waprave@gmail.com>
2024-08-12 13:25:05 -0400

    Moved BackendInfo types to backends

--- Code/GAL/Sedulous.GAL.Android/src/AndroidRuntime.cs
+++ Code/GAL/Sedulous.GAL.Android/src/AndroidRuntime.cs
@@ -1,20 +1,19 @@
-﻿using System;
-using System.Runtime.InteropServices;
+using System;
 
-namespace Veldrid.Android
+namespace Sedulous.GAL.Android
 {
     /// <summary>
     /// Function imports from the Android runtime library (android.so).
     /// </summary>
     internal static class AndroidRuntime
     {
-        private const string LibName = "android.so";
+        private const String LibName = "android.so";
 
-        [DllImport(LibName)]
-        public static extern IntPtr ANativeWindow_fromSurface(IntPtr jniEnv, IntPtr surface);
-        [DllImport(LibName)]
-        public static extern int32 ANativeWindow_setBuffersGeometry(IntPtr aNativeWindow, int32 width, int32 height, int32 format);
-        [DllImport(LibName)]
-        public static extern void ANativeWindow_release(IntPtr aNativeWindow);
+        [Import(LibName)]
+        public static extern void* ANativeWindow_fromSurface(void* jniEnv, void* surface);
+        [Import(LibName)]
+        public static extern int32 ANativeWindow_setBuffersGeometry(void* aNativeWindow, int32 width, int32 height, int32 format);
+        [Import(LibName)]
+        public static extern void ANativeWindow_release(void* aNativeWindow);
     }
 }
